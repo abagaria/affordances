36366886a879f73d7f6d8acec251f6f27b449d79
Akhil Bagaria <abagaria@hmc.edu>
2023-02-10 16:22:00 -0500

    Added a binary convolutional initiation classifier

---
+++ affordances/init_learners/classification/binary_init_classifier.py
@@ -0,0 +1,71 @@
+import torch
+import itertools
+import collections
+import numpy as np
+
+from affordances.utils import utils
+from affordances.init_learners.init_learner import InitiationLearner
+from affordances.init_learners.classification.conv_binary_classifier import ConvClassifier
+
+
+class ConvInitiationClassifier(InitiationLearner):
+  def __init__(self,
+    device, 
+    optimistic_threshold: float,
+    pessimistic_threshold: float,
+    n_input_channels: int = 1,
+    maxlen: int = 20
+  ):
+    self.device = device
+    self.optimistic_threshold = optimistic_threshold
+    self.pessimistic_threshold = pessimistic_threshold
+    self.n_input_channels = n_input_channels
+    
+    self.positive_examples = collections.deque([], maxlen=maxlen)
+    self.negative_examples = collections.deque([], maxlen=maxlen)
+    self.classifier = ConvClassifier(device, None, n_input_channels)
+    
+    super().__init__()
+
+  def _predict(self, states: np.ndarray, threshold) -> np.ndarray:
+    state_tensor = utils.tensorfy(states, self.device)
+    preprocessed_tensor = state_tensor / 255.
+    predictions = self.classifier.predict(preprocessed_tensor, threshold)
+    predictions = predictions.cpu().numpy()
+    if predictions.shape == (1, 1):
+      return predictions.squeeze()
+    return predictions
+
+  def optimistic_predict(self, states: np.ndarray) -> np.ndarray:
+    return self._predict(states, self.optimistic_threshold)
+
+  def pessimistic_predict(self, states: np.ndarray) -> np.ndarray:
+    return self._predict(states, self.pessimistic_threshold)
+
+  def add_trajectory(self, trajectory, success_label):
+    infos = [transition[-1] for transition in trajectory]
+    observations = [transition[-2] for transition in trajectory]
+
+    examples = [(obs, info) for obs, info in zip(observations, infos)]
+
+    if success_label:
+      self.positive_examples.append(examples)
+    else:
+      self.negative_examples.append(examples)
+
+  def update(self):
+    if self.positive_examples and self.negative_examples:
+      pos_examples = list(itertools.chain.from_iterable(self.positive_examples))
+      neg_examples = list(itertools.chain.from_iterable(self.negative_examples))
+      
+      pos_observations = [eg[0] for eg in pos_examples]
+      neg_observations = [eg[0] for eg in neg_examples]
+
+      positive_labels = torch.ones((len(pos_observations),))
+      negative_labels = torch.zeros((len(neg_observations),))
+
+      X = pos_observations + neg_observations
+      Y = torch.cat((positive_labels, negative_labels), dim=0)
+
+      if self.classifier.should_train(Y):
+        self.classifier.fit(X, Y)
---
+++ affordances/init_learners/classification/conv_binary_classifier.py
@@ -0,0 +1,129 @@
+import torch
+import random
+import numpy as np
+import torch.nn.functional as F
+
+from affordances.utils import utils
+from pfrl.nn.atari_cnn import SmallAtariCNN
+from torch.utils.data import Dataset, DataLoader
+
+
+class ImageCNN(torch.nn.Module):
+  def __init__(self, n_input_channels: int = 3):
+    super().__init__()
+
+    self.model = torch.nn.Sequential(
+      SmallAtariCNN(n_input_channels, n_output_channels=256),
+      torch.nn.Linear(in_features=256, out_features=128),
+      torch.nn.ReLU(),
+      torch.nn.Linear(in_features=128, out_features=1)
+    )
+
+  def forward(self, x):
+    assert x.max() <= 1.1, (x.max(), x.dtype)
+    return self.model(x.float())
+
+
+class ConvClassifier:
+  """"Generic weighted binary convolutional classifier."""
+  def __init__(self,
+        device,
+        threshold=0.5,
+        n_input_channels=1,
+        batch_size=32,
+        lr=1e-3):
+    
+    self.device = device
+    self.is_trained = False
+    self.threshold = threshold
+    self.batch_size = batch_size
+
+    self.model = ImageCNN(n_input_channels).to(device)
+    self.optimizer = torch.optim.Adam(self.model.parameters(), lr=lr)
+
+    # Debug variables
+    self.losses = []
+
+  @torch.no_grad()
+  def predict(self, X, threshold=None):
+    logits = self.model(X)
+    probabilities = torch.sigmoid(logits)
+    threshold = self.threshold if threshold is None else threshold
+    return probabilities > threshold
+
+  def determine_pos_weight(self, y):
+    n_negatives = len(y[y != 1])
+    n_positives = len(y[y == 1])
+    if n_positives > 0:
+      pos_weight = (1. * n_negatives) / n_positives
+      return torch.as_tensor(pos_weight).float()
+
+  def should_train(self, y):
+    enough_data = len(y) > self.batch_size
+    has_positives = len(y[y == 1]) > 0
+    has_negatives = len(y[y != 1]) > 0
+    return enough_data and has_positives and has_negatives
+
+  def preprocess_batch(self, X):
+    assert X.dtype == torch.uint8, X.dtype
+    return X.float() / 255.
+
+  def fit(self, X, y, n_epochs=1):
+    dataset = ClassifierDataset(X, y)
+    dataloader = DataLoader(dataset, batch_size=self.batch_size, shuffle=True)
+
+    if self.should_train(y):
+      losses = []
+
+      for _ in range(n_epochs):
+        epoch_loss = self._train(dataloader)                
+        losses.append(epoch_loss)
+      
+      self.is_trained = True
+
+      mean_loss = np.mean(losses)
+      self.losses.append(mean_loss)
+
+  def _train(self, loader):
+    """ Single epoch of training. """
+    batch_losses = []
+
+    for sampled_inputs, sampled_labels in loader:
+      sampled_inputs = utils.tensorfy(sampled_inputs, self.device)
+      sampled_inputs = self.preprocess_batch(sampled_inputs)
+      sampled_labels = sampled_labels.to(self.device)
+      
+      pos_weight = self.determine_pos_weight(sampled_labels)
+
+      # TODO(ab): This is inefficient: we sample many batches before a valid one
+      if not pos_weight:
+        continue
+
+      logits = self.model(sampled_inputs)
+      
+      loss = F.binary_cross_entropy_with_logits(
+        logits.squeeze(),
+        sampled_labels,
+        pos_weight=pos_weight
+      ) 
+
+      self.optimizer.zero_grad()
+      loss.backward()
+      self.optimizer.step()
+
+      batch_losses.append(loss.item())
+    
+    return np.mean(batch_losses)
+
+
+class ClassifierDataset(Dataset):
+  def __init__(self, states, labels):
+    self.states = states
+    self.labels = labels
+    super().__init__()
+
+  def __len__(self):
+    return len(self.labels)
+
+  def __getitem__(self, i):
+    return self.states[i], self.labels[i]
