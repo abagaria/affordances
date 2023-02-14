0add40bcfc58860bc8b3138d5abefe19de3732ad
Akhil Bagaria <abagaria@hmc.edu>
2023-02-14 17:17:17 -0500

    New base class for init *classifiers*

---
+++ affordances/init_learners/classification/init_classifier.py
@@ -0,0 +1,26 @@
+import collections
+import numpy as np
+
+from affordances.init_learners.init_learner import InitiationLearner
+
+
+class InitiationClassifier(InitiationLearner):
+  """Base class for the classification approach to init-learning."""
+
+  def __init__(self, max_n_trajectories):
+    super().__init__()
+
+    self.positive_examples = collections.deque([], maxlen=max_n_trajectories)
+    self.negative_examples = collections.deque([], maxlen=max_n_trajectories)
+
+  def optimistic_predict(self, states: np.ndarray) -> np.ndarray:
+    raise NotImplementedError()
+
+  def pesssimistic_predict(self, states: np.ndarray) -> np.ndarray:
+    raise NotImplementedError()
+
+  def add_trajectory(self, trajectory):
+    raise NotImplementedError()
+  
+  def update(self):
+    raise NotImplementedError()
