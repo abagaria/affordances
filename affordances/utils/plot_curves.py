0fa0f2290b2d7a957b508d4c7944998e48d3b3b7
Ben Abbatematteo <abba@brown.edu>
2023-03-23 16:10:11 -0400

    plot classifier loss, final grasp scores, final grasp counts

--- affordances/utils/plot_curves.py
+++ affordances/utils/plot_curves.py
@@ -1,6 +1,7 @@
 import os 
 import argparse
 import pickle 
+import numpy as np 
 import pandas as pd
 
 import gzip
@@ -12,13 +13,35 @@ if __name__ == '__main__':
   args = parser.parse_args()
 
   data = pickle.load(gzip.open(args.path, 'rb+'))
+  losses = data.pop('classifier_loss')
+  scores = data.pop('scores')
+  counts = data.pop('counts')
+
   df = pd.DataFrame(data)
   smooth = df.ewm(com=10).mean()
 
   plt.plot(smooth['rewards'])
+  plt.title('Return')
   plt.show()
 
   plt.plot(smooth['success'])
+  plt.title('Success')
   plt.show()
+
+  plt.plot(losses)
+  plt.title('Loss')
+  plt.show()  
+
+  plt.subplot(121)
+  plt.bar(np.arange(len(scores)), scores)
+  plt.title('Grasp Scores')
+
+  plt.subplot(122)
+  plt.bar(counts.keys(), counts.values())
+  plt.title('Grasp Counts')
+  plt.show()
+
+
+  
 
 	
