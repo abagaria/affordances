0bd81f81bede53ec1cd1edc325250c6b824902e6
Akhil Bagaria <abagaria@hmc.edu>
2023-03-30 13:42:12 -0400

    weighted binary classifier

--- affordances/init_learners/classification/binary_init_classifier.py
+++ affordances/init_learners/classification/binary_init_classifier.py
@@ -51,7 +51,7 @@ class ConvInitiationClassifier(InitiationClassifier):
     else:
       self.negative_examples.append(examples)
 
-  def update(self):
+  def update(self, initiation_gvf=None, goal=None):
     if self.positive_examples and self.negative_examples:
       pos_examples = list(itertools.chain.from_iterable(self.positive_examples))
       neg_examples = list(itertools.chain.from_iterable(self.negative_examples))
@@ -66,7 +66,7 @@ class ConvInitiationClassifier(InitiationClassifier):
       Y = torch.cat((positive_labels, negative_labels), dim=0)
 
       if self.classifier.should_train(Y):
-        self.classifier.fit(X, Y)
+        self.classifier.fit(X, Y, initiation_gvf, goal)
 
   def save(self, filename: str):
     torch.save(self.classifier.model.state_dict(), filename)
