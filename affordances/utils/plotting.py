22290e30218eaabdfcdc8e1d7f01361ec1080ecc
Akhil Bagaria <abagaria@hmc.edu>
2023-03-30 13:38:06 -0400

    [plotting] better plotting of weighted-classifiers.

--- affordances/utils/plotting.py
+++ affordances/utils/plotting.py
@@ -2,20 +2,19 @@ import ipdb
 import collections
 import numpy as np
 import matplotlib.pyplot as plt
+from affordances.utils import utils
 from pfrl.replay_buffers.prioritized import PrioritizedReplayBuffer
+from affordances.init_learners.gvf.init_gvf import InitiationGVF
 
 
 def parse_replay(replay):
-  states = []
-  x_locations = []
-  y_locations = []
+  state_dict = {}
   memory = replay.memory.data if isinstance(replay, PrioritizedReplayBuffer) else replay.memory
   for transition in memory:
     transition = transition[-1]  # n-step to single transition
-    states.append(transition['next_state'])
-    x_locations.append(transition['extra_info']['player_x'])
-    y_locations.append(transition['extra_info']['player_y'])
-  return states, x_locations, y_locations
+    pos = transition['extra_info']['player_x'], transition['extra_info']['player_y']
+    state_dict[pos] = transition['next_state']
+  return state_dict
 
 
 def visualize_value_func(agent, params, replay, episode, experiment_name, seed):
@@ -108,4 +107,115 @@ def visualize_initiation_set(option, replay, episode, experiment_name, seed):
 
   plt.savefig(f'plots/{experiment_name}/{seed}/{option}_init_{episode}.png')
   plt.close()
-  # return x, y, pv, ov
+  
+  
+def visualize_gc_initiation_learner(
+    initiation_learner, replay, goal, goal_info,
+    episode, plot_base_dir, experiment_name, seed,
+    save_fig=True
+  ):
+  
+  state_dict = parse_replay(replay)
+  value_dict = {}
+  for pos in state_dict:
+    nsg = state_dict[pos]
+    obs = nsg[0][None, ...]
+    assert obs.shape == (1, 84, 84), obs.shape
+    value = initiation_learner.get_values([obs], [goal])
+    value_dict[pos] = value.item()
+  
+  x = [pos[0] for pos in value_dict]
+  y = [pos[1] for pos in value_dict]
+  v = [value_dict[pos] for pos in value_dict]
+  plt.scatter(x, y, c=v)
+  plt.colorbar()
+  plt.title(f'Goal pos: {goal_info["player_pos"]}')
+  if save_fig:
+    plt.savefig(f'{plot_base_dir}/{experiment_name}/init_vf_{seed}_{episode}.png')
+    plt.close()
+  return value_dict
+
+
+def offline_visuzlize_gc_initiation_learner(
+    path_to_init_learner,
+    n_actions,
+    goal, goal_info,
+    episode, plot_base_dir, experiment_name, seed
+  ):
+  """Visualize the init learner when you just have access to the saved model."""
+  assert '.pth' in path_to_init_learner
+  init_learner = InitiationGVF(
+    target_policy=None,
+    n_actions=n_actions,
+    n_input_channels=1,
+  )
+  init_learner.load(path_to_init_learner)
+  visualize_gc_initiation_learner(
+    init_learner, init_learner.initiation_replay_buffer,
+    goal, goal_info, episode, plot_base_dir, experiment_name, seed
+  )
+
+
+def visualize_initiation_classifier(
+  initiation_classifier, init_gvf, replay, goal, goal_info,
+  option_name, episode, plot_base_dir, experiment_name, seed
+):
+  state_dict = parse_replay(replay)
+  value_dict = {}
+  for pos in state_dict:
+    nsg = state_dict[pos]
+    obs = nsg[0][None, ...][None, ...]
+    assert obs.shape == (1, 1, 84, 84), obs.shape  # [B, C, H, W]
+    value = initiation_classifier.optimistic_predict(obs)
+    value_dict[pos] = value.item()
+  
+  x = [pos[0] for pos in value_dict]
+  y = [pos[1] for pos in value_dict]
+  v = [value_dict[pos] for pos in value_dict]
+
+  plt.figure(figsize=(20, 12))
+  plt.subplot(2, 2, 1)
+  plt.scatter(x, y, c=v)
+  plt.colorbar()
+  pos = goal_info["player_pos"]
+  plt.title(f'Classifier: Goal pos: {pos}')
+
+  # Save the lims for plotting egs later
+  xlim = min(x), max(x)
+  ylim = min(y), max(x)
+
+  # Plot the initiation GVF
+  plt.subplot(2, 2, 2)
+  pos2value = visualize_gc_initiation_learner(
+    init_gvf, replay, goal, goal_info, episode,
+    plot_base_dir, experiment_name, seed, save_fig=False)
+
+  # Plot the training examples
+  plt.subplot(2, 2, 3)
+  positives = utils.flatten(initiation_classifier.positive_examples)
+  negatives = utils.flatten(initiation_classifier.negative_examples)
+  
+  x_positives = [eg[1]['player_x'] for eg in positives]
+  y_positives = [eg[1]['player_y'] for eg in positives]
+  x_negatives = [eg[1]['player_x'] for eg in negatives]
+  y_negatives = [eg[1]['player_y'] for eg in negatives]
+
+  weights_positives = [pos2value[eg[1]['player_pos']] for eg in positives]
+  weights_negatives = [(1 - pos2value[eg[1]['player_pos']]) for eg in negatives]
+  
+  plt.scatter(x_positives, y_positives, c=weights_positives)
+  plt.xlim(xlim)
+  plt.ylim(ylim)
+  plt.colorbar()
+  plt.title('Clf Positive Examples')
+
+  plt.subplot(2, 2, 4)
+  plt.scatter(x_negatives, y_negatives, c=weights_negatives)
+  plt.xlim(xlim)
+  plt.ylim(ylim)
+  plt.colorbar()
+  plt.title('Clf Negative Examples')
+
+  filename = f'{option_name}_init_vf_{pos}_{seed}_{episode}.png'
+  plt.savefig(f'{plot_base_dir}/{experiment_name}/{filename}')
+  plt.close()
