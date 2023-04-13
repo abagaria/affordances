84427c551a4de3d1393ae7716e1e0444fedd9059
Akhil Bagaria <abagaria@hmc.edu>
2023-04-13 16:42:28 -0400

    Added options with pre-specified terminations.

---
+++ affordances/agent/hrl/option.py
@@ -0,0 +1,203 @@
+from __future__ import annotations
+
+import ipdb
+import random
+import numpy as np
+import collections
+
+from affordances.utils import utils
+from affordances.agent.rainbow.rainbow import Rainbow
+from affordances.init_learners.gvf.init_gvf import GoalConditionedInitiationGVF
+from affordances.goal_attainment.attainment_classifier import GoalAttainmentClassifier
+from affordances.init_learners.classification.binary_init_classifier import ConvInitiationClassifier
+
+
+class Option:
+  def __init__(self,
+    option_idx: int,
+    uvfa_policy: Rainbow,
+    initiation_gvf: GoalConditionedInitiationGVF,
+    goal_attainment_classifier: GoalAttainmentClassifier,
+    gestation_period: int,
+    timeout: int,
+    exploration_bonus_scale: float,
+    use_her_for_policy_evaluation: bool,
+    use_weighted_classifiers: bool,
+    subgoal_obs: np.ndarray,
+    subgoal_info: dict):
+      self._timeout = timeout
+      self._solver = uvfa_policy
+      self._option_idx = option_idx
+      self._gestation_period = gestation_period
+      self._goal_attainment_classifier = goal_attainment_classifier
+      self._exploration_bonus_scale = exploration_bonus_scale
+      self._use_her_for_policy_evaluation = use_her_for_policy_evaluation
+      self._use_weighted_classifiers = use_weighted_classifiers
+      
+      self.initiation_gvf = initiation_gvf
+
+      self.is_last_option = False
+      self.expansion_classifier = None
+
+      self._is_global_option = option_idx == 0
+      
+      self.num_goal_hits = 0
+      self.num_executions = 0
+      self.effect_set = collections.deque(maxlen=20)
+
+      # Information for logging and debugging
+      self.debug_log = collections.defaultdict(list)
+
+      self.initiation_classifier = ConvInitiationClassifier(
+        device=uvfa_policy.device,
+        optimistic_threshold=0.5,
+        pessimistic_threshold=0.75,
+        n_input_channels=1,
+      )
+
+      self.subgoal_obs = subgoal_obs
+      self.subgoal_info = subgoal_info
+      self.termination_classifier = lambda info: self._goal_attainment_classifier(info, self.subgoal_info)
+
+      print(f'Created {self} with bonus_scale={exploration_bonus_scale}',
+            f'using_weighted_binary_classifier={use_weighted_classifiers}',
+            f'with subgoal={subgoal_info}')
+
+  @property
+  def training_phase(self):
+    return 'gestation' if self.num_goal_hits < self._gestation_period else 'mature'
+  
+  def optimistic_is_init_true(self, state, info):
+    if self._is_global_option or self.training_phase == 'gestation':
+      return True
+    
+    return self.initiation_classifier.optimistic_predict([state]) or \
+           self.initiation_classifier.pessimistic_predict([state])
+
+  def pessimistic_is_init_true(self, state, info):
+    return self.initiation_classifier.pessimistic_predict([state])
+
+  def is_term_true(self, state, info):
+    return self.termination_classifier(info)  # or info['terminated']
+
+  def act(self, state, goal):
+    augmented_state = self.get_augmeted_state(state, goal)
+    return self._solver.act(augmented_state)
+
+  def rollout(self, env, state, info):
+    """Execute the option policy from `state` towards `goal`."""
+       
+    done = False
+    reset = False
+    reached = False
+
+    n_steps = 0
+    total_reward = 0.
+    trajectory = []  # (s, a, r, s', info)
+
+    while not done and not reset and not reached and n_steps < self._timeout:
+      action = self.act(state, self.subgoal_obs)
+      
+      next_state, reward, done, info = env.step(action)
+
+      n_steps += 1
+      trajectory.append((state, action, reward, next_state, info))
+      reached = self.is_term_true(next_state, info)
+
+      state = next_state
+      reset = info['needs_reset']
+      total_reward += reward
+
+    self.log_progress(info, reached, self.subgoal_info)
+
+    return state, info, total_reward, reached, n_steps, trajectory
+  
+  def update_option_after_rollout(self, transitions, success):
+    if success:
+      self.num_goal_hits += 1
+
+      final_state = transitions[-1][-2]
+      final_info = transitions[-1][-1]
+
+      self.effect_set.append((final_state, final_info))
+
+    self.update_option_params(transitions, self.subgoal_obs, self.subgoal_info)
+
+    if not self._is_global_option:
+      self.update_option_initiation_classifiers(transitions, success, self.subgoal_obs)
+
+  def update_option_params(self, transitions, goal, goal_info):
+    """Update the parameters of the UVFA and the initiation GVF."""
+
+    self._solver.experience_replay(
+      self.relabel_trajectory(transitions, goal, goal_info, add_bonus=True))
+    self.initiation_gvf.add_trajectory_to_replay(
+      self.relabel_trajectory(transitions, goal, goal_info, add_bonus=False))
+
+    hindsight_goal, hindsight_goal_info = self.pick_hindsight_goal(transitions)
+
+    # If the first state in the trajectory doesn't achieve the hindsight goal
+    if not self._goal_attainment_classifier(transitions[0][-1], hindsight_goal_info):
+      self._solver.experience_replay(
+        self.relabel_trajectory(
+        transitions, hindsight_goal, hindsight_goal_info, add_bonus=True))
+      
+      if self._use_her_for_policy_evaluation:
+        self.initiation_gvf.add_trajectory_to_replay(
+          self.relabel_trajectory(
+          transitions, hindsight_goal, hindsight_goal_info, add_bonus=False))
+
+  def relabel_trajectory(self, transitions, goal, goal_info, add_bonus=True):
+    relabeled_trajectory = []
+    bonus_scale = self._exploration_bonus_scale if add_bonus else 0
+    for state, action, _, next_state, info in transitions:
+      sg = self.get_augmeted_state(state, goal)
+      nsg = self.get_augmeted_state(next_state, goal)
+      reached = self._goal_attainment_classifier(info, goal_info)
+      reward = float(reached) + (bonus_scale * info['bonus'])
+      relabeled_trajectory.append((sg, action, reward, nsg, reached, info))
+      if reached:
+        break
+    return relabeled_trajectory
+
+  def pick_hindsight_goal(self, transitions, method='final'):
+    if method == 'final':
+      goal_transition = transitions[-1]
+      goal = goal_transition[-2]
+      goal_info = goal_transition[-1]
+      return goal, goal_info
+    if method == 'future':
+      start_idx = len(transitions) // 2
+      goal_idx = random.randint(start_idx, len(transitions) - 1)
+      goal_transition = transitions[goal_idx]
+      goal = goal_transition[-2]
+      goal_info = goal_transition[-1]
+      return goal, goal_info
+    raise NotImplementedError(method)
+
+  def update_option_initiation_classifiers(self, transitions, success, goal):
+    self.initiation_classifier.add_trajectory(transitions, success)
+    
+    if self._use_weighted_classifiers:
+      self.initiation_classifier.update(self.initiation_gvf, goal)
+    else:
+      self.initiation_classifier.update()
+  
+  def get_augmeted_state(self, state, goal):
+    assert state.shape == (1, 84, 84) or state.shape == (1, 64, 64), state.shape
+    return np.concatenate((state, goal), axis=0)
+  
+  def log_progress(self, info, success, goal_info):
+    self.debug_log['timesteps'].append(info['timestep'])
+    self.debug_log['successes'].append(success)
+    self.debug_log['goals'].append(goal_info)
+    self.debug_log['phases'].append(self.training_phase)
+
+  def __hash__(self) -> int:
+    return self._option_idx
+
+  def __str__(self) -> str:
+    return f'option-{self._option_idx}'
+  
+  def __repr__(self) -> str:
+    return str(self)
---
+++ affordances/experiments/gridworld_init_accuracy/options.py
@@ -0,0 +1,99 @@
+"""Create options for visual gridworld."""
+
+
+from affordances.agent.hrl.option import Option
+from affordances.agent.rainbow.rainbow import Rainbow
+from affordances.init_learners.gvf.init_gvf import GoalConditionedInitiationGVF
+from affordances.goal_attainment.attainment_classifier import DiscreteInfoAttainmentClassifier
+
+
+class AgentOverOptions:
+  def __init__(
+    self,
+    env,
+    gestation_period,
+    timeout,
+    gpu: int = 0,
+    n_input_channels: int = 1,
+    env_steps: int = int(500_000),
+    epsilon_decay_steps: int = 25_000,
+    final_epsilon: float = 0.1
+  ):
+    self._env = env
+    self._timeout = timeout
+    self._gestation_period = gestation_period
+    self._gpu = gpu
+    self._n_input_channels = n_input_channels
+    
+    self.uvfa_policy = self.create_uvfa_policy(
+      env_steps, epsilon_decay_steps, final_epsilon)
+    
+    self.initiation_gvf = self.create_initiation_learner()
+    self.subgoals = self.get_subgoals()
+    self.options = self.create_options()  # TODO: create global option
+
+  def update_initiation_gvf(self, episode_duration):
+    """SGD steps for the initiation GVF."""
+    self.initiation_gvf.update(
+      n_updates=(episode_duration // self.uvfa_policy.update_interval)
+    )
+
+  def create_uvfa_policy(self, env_steps, epsilon_decay_steps, final_eps):
+    kwargs = dict(
+      n_atoms=51, v_max=10., v_min=-10.,
+      noisy_net_sigma=0.5,
+      lr=1e-4,
+      n_steps=3,
+      betasteps=env_steps // 4,
+      replay_start_size=1024, 
+      replay_buffer_size=int(3e5),
+      gpu=self._gpu,
+      n_obs_channels=2*self._n_input_channels,
+      use_custom_batch_states=False,
+      final_epsilon=final_eps,
+      epsilon_decay_steps=epsilon_decay_steps
+    )
+    return Rainbow(self._env.action_space.n, **kwargs)
+  
+  def create_initiation_learner(self):
+    """Common goal-conditioned initiation learner shared by all options."""
+    return GoalConditionedInitiationGVF(
+      target_policy=self.uvfa_policy.agent.batch_act,
+      n_actions=self._env.action_space.n,
+      n_input_channels=2*self._n_input_channels,
+      optimistic_threshold=0.5,
+      pessimistic_threshold=0.75  # don't need this
+    )
+  
+  def get_subgoal_positions_for_6x6_gridworld(self):
+    return [(0, 5), (5, 0), (3, 3), (5, 5)]
+  
+  def get_subgoal_positions_for_13x13_gridworld(self):
+    return [(0,12), (12,0), (6,6), (12,12)]
+ 
+  def get_subgoals(self):
+    pos2goals = {}  # pos -> (obs, info)
+    subgoal_positions = self.get_subgoal_positions_for_6x6_gridworld()
+    for pos in subgoal_positions:
+      obs, info = self._env.reset(pos=pos)
+      pos2goals[pos] = (obs, info)
+    return pos2goals
+
+  def create_options(self) -> list:
+    options = []
+    goal_attainment_clf = DiscreteInfoAttainmentClassifier("player_pos")
+    for i, pos in enumerate(self.subgoals):
+      subgoal_obs, subgoal_info = self.subgoals[pos]
+      option = Option(i + 1,  # Assuming that none of these are global-option
+                      self.uvfa_policy,
+                      self.initiation_gvf,
+                      goal_attainment_clf,
+                      self._gestation_period,
+                      self._timeout,
+                      exploration_bonus_scale=0,
+                      use_her_for_policy_evaluation=True,
+                      use_weighted_classifiers=True,
+                      subgoal_obs=subgoal_obs,
+                      subgoal_info=subgoal_info)
+      options.append(option)
+    return options
