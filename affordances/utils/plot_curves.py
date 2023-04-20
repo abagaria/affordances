3bcaae58812529c941e68f3b8051027e3f3b98ad
Ben Abbatematteo <abba@brown.edu>
2023-04-20 11:56:14 -0400

    updates to plots

--- affordances/utils/plot_curves.py
+++ affordances/utils/plot_curves.py
@@ -16,32 +16,42 @@ if __name__ == '__main__':
   losses = data.pop('classifier_loss')
   scores = data.pop('scores')
   counts = data.pop('counts')
+  success = data.pop('grasp_success')
+  rates = np.array(list(success.values())) / np.array(list(counts.values()))
 
-  df = pd.DataFrame(data)
-  smooth = df.ewm(com=10).mean()
+  # df = pd.DataFrame(data)
+  # smooth = df.ewm(com=10).mean()
 
-  plt.plot(smooth['rewards'])
-  plt.title('Return')
-  plt.show()
+  # plt.plot(smooth['rewards'])
+  # plt.title('Return')
+  # plt.show()
 
-  plt.plot(smooth['success'])
-  plt.title('Success')
-  plt.show()
+  # plt.plot(smooth['success'])
+  # plt.title('Success')
+  # plt.show()
 
-  plt.plot(losses)
-  plt.title('Loss')
-  plt.show()  
-
-  plt.subplot(121)
-  plt.bar(np.arange(len(scores)), scores)
-  plt.title('Grasp Scores')
-
-  plt.subplot(122)
+  # plt.plot(losses)
+  # plt.title('Loss')
+  # plt.show()  
+  plt.figure(figsize=(24,10))
+  plt.subplot(311)
   plt.bar(counts.keys(), counts.values())
-  plt.title('Grasp Counts')
+  plt.title('Grasp Attempt Counts')
+
+  plt.subplot(312)
+  plt.bar(success.keys(), rates)
+  plt.title('Grasp Success Rates')
+
+  plt.subplot(313)
+  plt.bar(np.arange(len(scores)), scores)
+  plt.title('Final Grasp Scores')
+
+  plt.suptitle(os.path.dirname(args.path).split("/")[-1])
+  plt.savefig(os.path.join(os.path.dirname(args.path), 'hist.png'))
+  print(os.path.join(os.path.dirname(args.path), 'hist.png'))
   plt.show()
 
 
   
 
-	
+	# python -m affordances.utils.plot_curves results/robot_4_10/robot_4_10_081__environmentname_DoorCIP__initlearner_binary__seed_1__optimalik_False/log_seed1.pkl
