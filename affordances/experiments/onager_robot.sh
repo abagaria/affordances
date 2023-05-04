4e3d0273a8dbafb8926f106adeae1c58736d8107
Ben Abbatematteo <abba@brown.edu>
2023-05-04 15:04:45 -0400

    vis init set test exp

--- affordances/experiments/onager_robot.sh
+++ affordances/experiments/onager_robot.sh
@@ -1,11 +1,11 @@
-onager prelaunch +jobname robot_5_3_gvf \
+onager prelaunch +jobname robot_5_4_vis_door \
     +command "python -m affordances.experiments.robot_td3 \
-    --experiment_name=robot_5_3_gvf \
+    --experiment_name=robot_5_4_vis \
     --n_episodes=5000 \
     --log_dir=/gpfs/data/gdk/babbatem/" \
-    +arg --environment_name DoorCIP LeverCIP SlideCIP \
-    +arg --init_learner gvf weighted-binary \
+    +arg --environment_name DoorCIP \
+    +arg --init_learner binary gvf weighted-binary \
     +arg --seed 1 2 3 4 5 6 7 8 9 10 \
-    +arg --optimal_ik True False \
+    +arg --optimal_ik False \
     +arg --segment False \
     +tag --sub_dir
