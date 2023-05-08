898d40e0ed4b8fa4e72be1a494c43c74c771186b
Ben Abbatematteo <abba@brown.edu>
2023-05-08 15:16:20 -0400

    true random selection

--- affordances/init_learners/classification/random_grasp_baseline.py
+++ affordances/init_learners/classification/random_grasp_baseline.py
@@ -3,7 +3,10 @@ import numpy as np
 class RandomGraspBaseline:
   def score(self, states: np.ndarray) -> np.ndarray:
     n_states = len(states)
-    return np.random.rand(n_states)
+    scores = np.zeros(n_states)
+    pick = np.random.randint(0, n_states)
+    scores[pick] = 1.0
+    return scores
 
   def update(self, initiation_gvf=None):
     pass 
