77c68322cfad6d0f21aaca1b3db022d4fefe1512
Ben Abbatematteo <abba@brown.edu>
2023-05-11 17:33:59 -0400

    slide long runs

--- affordances/experiments/onager_robot.sh
+++ affordances/experiments/onager_robot.sh
@@ -1,16 +1,61 @@
-onager prelaunch +jobname 5-9-refit \
+onager prelaunch +jobname 5-11-slide-class \
     +command "python -m affordances.experiments.robot_td3 \
-    --experiment_name=5-9-refit \
-    --n_episodes=5000 \
+    --experiment_name=5-11-slide-class \
+    --n_episodes=10000 \
     --log_dir=/gpfs/data/gdk/babbatem/ \
     --vis_init_set False" \
-    +arg --environment_name DoorCIP SlideCIP LeverCIP \
+    +arg --environment_name SlideCIP \
     +arg --init_learner binary weighted-binary \
-    +arg --seed 1 2 3 4 5 6 7 8 9 10 \
+    +arg --seed 1 2 3 4 5 \
     +arg --optimal_ik False \
     +arg --segment False \
     +arg --sampler sum \
     +arg --uncertainty none \
     +arg --only_reweigh_negatives True False \
-    +arg --gestation 5 10 \
+    +arg --gestation 1 5 10 \
+    +tag --sub_dir
+
+onager prelaunch +jobname 5-11-slide-gvf-counts \
+    +command "python -m affordances.experiments.robot_td3 \
+    --experiment_name=5-11-slide-gvf-counts \
+    --n_episodes=10000 \
+    --log_dir=/gpfs/data/gdk/babbatem/ \
+    --vis_init_set False" \
+    +arg --environment_name SlideCIP \
+    +arg --init_learner gvf \
+    +arg --seed 1 2 3 4 5 \
+    +arg --optimal_ik False \
+    +arg --segment False \
+    +arg --sampler sum \
+    +arg --uncertainty count_qpos count_grasp \
+    +arg --bonus_scale 0.05 0.1 0.25 0.5 1.0 \
+    +tag --sub_dir
+
+onager prelaunch +jobname 5-11-slide-gvf-none \
+    +command "python -m affordances.experiments.robot_td3 \
+    --experiment_name=5-11-slide-gvf-none \
+    --n_episodes=10000 \
+    --log_dir=/gpfs/data/gdk/babbatem/ \
+    --vis_init_set False" \
+    +arg --environment_name SlideCIP \
+    +arg --init_learner gvf \
+    +arg --seed 1 2 3 4 5 \
+    +arg --optimal_ik False \
+    +arg --segment False \
+    +arg --sampler sum \
+    +arg --uncertainty none \
+    +arg --bonus_scale 0.0 \
+    +tag --sub_dir
+
+onager prelaunch +jobname 5-11-slide-rand \
+    +command "python -m affordances.experiments.robot_td3 \
+    --experiment_name=5-11-slide-rand \
+    --n_episodes=10000 \
+    --log_dir=/gpfs/data/gdk/babbatem/ \
+    --vis_init_set False" \
+    +arg --environment_name SlideCIP \
+    +arg --init_learner random \
+    +arg --seed 1 2 3 4 5 \
+    +arg --optimal_ik False \
+    +arg --segment False \
     +tag --sub_dir
